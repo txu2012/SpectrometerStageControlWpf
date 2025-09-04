a66b432f37b6bea903d196966cd52b4161526e5f
Tony Xu <tony.xu@santec.com>
2025-09-04 16:26:57 +0900

    WIP - Converting to WPF

---
+++ SpectrometerStageControlWpf/Control/TesterExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectrometerStageControlWpf
+{
+    public class TesterExtension
+    {
+
+        public static List<FrogData> GenerateTestFrogDataSet()
+        {
+            Random random = new Random();
+
+            double[] wavelengthsTest = { 10, 15, 20, 25, 30, 35, 40, 45, 50, 55 };
+
+            FrogHeaderData header = new FrogHeaderData();
+            header.NumDelayPoints = 20;
+            header.NumWavelengthPoints = 10;
+            header.DelayIncrements = 1.0;
+            header.WavelengthIncrements_nm = 5.0;
+            header.WavelengthCenter = 30.0;
+
+            List<FrogData> data = new List<FrogData>();
+            for (int i = 0; i < 20; ++i)
+            {
+                // Generate different intensities for different delays
+                double[] intensitiesTest = Enumerable
+                    .Repeat(0, 10)
+                    .Select(j => random.NextDouble())
+                    .ToArray();
+
+                data.Add(new FrogData(i, new SpectrumData(wavelengthsTest, intensitiesTest)));
+            }
+
+            return data;
+        }
+    }
+}
--- SpectrometerStageControlWpf/MainWindow.xaml.cs
+++ SpectrometerStageControlWpf/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -18,11 +19,344 @@ namespace SpectrometerStageControlWpf
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IMainView
     {
+        #region Class Members
+        private MainPresenter presenter;
+        private bool updatingDisplay = false;
+        //private FormChart formChart;
+        //private Form3dPlot formPlot;
+
+        private DispatcherTimer tmrMain;
+        #endregion
         public MainWindow()
         {
+            this.presenter = new MainPresenter();
             InitializeComponent();
+
+            this.presenter.AddMainView(this);
+
+            tmrMain = new DispatcherTimer();
+            tmrMain.Tick += tmrMain_Tick;
+            tmrMain.Interval = new TimeSpan(0, 0, 0, 0, 50);
+
+            RefreshStage();
+            RefreshSpectrometer();
+            UpdateDisplay();
+        }
+
+        #region Interface functions
+        public void UpdateDisplay() 
+        {
+            updatingDisplay = true;
+
+            if (presenter.StageConnected)
+            {
+                btnStageConnect.IsEnabled = false;
+                btnStageRefresh.IsEnabled = false;
+                btnStageDisconnect.IsEnabled = true;
+                cbStage.IsEnabled = false;
+            }
+            else
+            {
+                btnStageConnect.IsEnabled = true;
+                btnStageRefresh.IsEnabled = true;
+                btnStageDisconnect.IsEnabled = false;
+                cbStage.IsEnabled = true;
+            }
+
+            if (presenter.SpectrometerConnected)
+            {
+                btnSpecConnect.IsEnabled = false;
+                btnSpecRefresh.IsEnabled = false;
+                btnSpecDisconnect.IsEnabled = true;
+                cbSpectrometer.IsEnabled = false;
+            }
+            else
+            {
+                btnSpecConnect.IsEnabled = true;
+                btnSpecRefresh.IsEnabled = true;
+                btnSpecDisconnect.IsEnabled = false;
+                cbSpectrometer.IsEnabled = true;
+            }
+
+            lblStageConn.Content = (presenter.StageConnected) ? "Connected" : "Disconnected";
+            lblSpectrometerConn.Content = (presenter.SpectrometerConnected) ? "Connected" : "Disconnected";
+
+            /*gbStage.IsEnabled = presenter.StageConnected;
+            gbSpectrometer.IsEnabled = presenter.SpectrometerConnected;
+
+            if (gbStage.IsEnabled)
+                UpdateStageDisplay();
+
+            if (gbSpectrometer.IsEnabled)
+                UpdateSpectrometerDisplay();*/
+
+            updatingDisplay = false;
+        }
+
+        private void UpdateStageDisplay() 
+        {
+            if (presenter.Stage.StageState != MotorState.Stopped)
+            {
+                btnHome.IsEnabled = false;
+                btnContBack.IsEnabled = false;
+                btnContFwd.IsEnabled = false;
+                nudTimeFs.IsEnabled = false;
+                nudTimeRangeFs.IsEnabled = false;
+                nudStageMoveBy.IsEnabled = false;
+                nudStageRange.IsEnabled = false;
+                btnMoveByNeg.IsEnabled = false;
+                btnMoveByPos.IsEnabled = false;
+                btnSetMm.IsEnabled = false;
+                btnSetMmRange.IsEnabled = false;
+            }
+            else
+            {
+                btnHome.IsEnabled = true;
+                btnContBack.IsEnabled = true;
+                btnContFwd.IsEnabled = true;
+                nudTimeFs.IsEnabled = true;
+                nudTimeRangeFs.IsEnabled = true;
+                nudStageMoveBy.IsEnabled = true;
+                nudStageRange.IsEnabled = true;
+                btnMoveByNeg.IsEnabled = true;
+                btnMoveByPos.IsEnabled = true;
+                btnSetMm.IsEnabled = true;
+                btnSetMmRange.IsEnabled = true;
+            }
+            txtHomed.Text = (presenter.Stage.IsHomed) ? "Yes" : "No";
+            txtPosition.Text = presenter.Stage.CurrentPosition.ToString();
+            txtState.Text = presenter.Stage.StageState.ToString();
+
+            nudStageMoveBy.Value = (double)presenter.MoveBy_mm;
+            nudStageRange.Value = (double)presenter.MoveRange_mm;
+            nudTimeFs.Value = (double)presenter.TimeMove_fs;
+            nudTimeRangeFs.Value = (double)presenter.TimeRange_fs;
+        }
+
+        private void UpdateSpectrometerDisplay() 
+        {
+            nudCenterWave.Value = (double)presenter.CenterWavelength;
+            nudWaveRange.Value = (double)presenter.WavelengthRange;
+            nudIntegrationUs.Value = (double)presenter.IntegrationTime_us;
+        }
+
+        public void Log(string msg) 
+        {
+            rtbLog.AppendText(msg + "\r\n");
+            rtbLog.ScrollToEnd();
+        }
+        #endregion
+
+        private void RefreshStage() 
+        {
+            cbStage.ItemsSource = presenter.StageDevices;
+        }
+
+        private void RefreshSpectrometer() 
+        {
+            presenter.RefreshSpectrometerDevices();
+            if (presenter.SpectrometerDevices.Count > 0)
+                cbSpectrometer.ItemsSource = presenter.SpectrometerDevices.Select(x => x.Id + "" + x.Name).ToList();
+        }
+
+        #region Connection
+        private void btnStageConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbStage.SelectedItem != null)
+                presenter.ConnectStage((string)cbStage.SelectedItem);
+            UpdateDisplay();
+        }
+
+        private void btnStageDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (presenter.StageConnected)
+                presenter.DisconnectStage();
+            UpdateDisplay();
+        }
+
+        private void btnStageRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshStage();
+            UpdateDisplay();
+        }
+
+        private void btnSpecConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbSpectrometer.SelectedItem != null)
+                presenter.ConnectSpectrometer(cbSpectrometer.SelectedIndex);
+            UpdateDisplay();
+        }
+
+        private void btnSpecDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (presenter.SpectrometerConnected)
+                presenter.DisconnectSpectrometer();
+            UpdateDisplay();
+        }
+
+        private void btnSpecRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshSpectrometer();
+            UpdateDisplay();
+        }
+        #endregion
+
+        #region Stage Control
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            presenter.StageHome();
+            tmrMain.IsEnabled = true;
+        }
+
+        private void btnContBack_Click(object sender, RoutedEventArgs e)
+        {
+            presenter.StageMoveContinuous(false);
+            tmrMain.IsEnabled = true;
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            presenter.StageStop();
+            tmrMain.IsEnabled = true;
+        }
+
+        private void btnContFwd_Click(object sender, RoutedEventArgs e)
+        {
+            presenter.StageMoveContinuous(true);
+            tmrMain.IsEnabled = true;
+        }
+
+        private void btnMoveByNeg_Click(object sender, RoutedEventArgs e)
+        {
+            presenter.StageMoveRelative(false, (decimal)nudStageMoveBy.Value);
+            tmrMain.IsEnabled = true;
+        }
+
+        private void btnMoveByPos_Click(object sender, RoutedEventArgs e)
+        {
+            presenter.StageMoveRelative(true, (decimal)nudStageMoveBy.Value);
+            tmrMain.IsEnabled = true;
+        }
+
+        private void nudStageMoveBy_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (updatingDisplay) return;
+            presenter.MoveBy_mm = (decimal)nudStageMoveBy.Value;
+            presenter.TimeMove_fs = presenter.MmToFemtosecond((decimal)nudStageMoveBy.Value);
+
+            UpdateDisplay();
+        }
+
+        private void nudStageRange_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (updatingDisplay) return;
+            presenter.MoveRange_mm = (decimal)nudStageRange.Value;
+            presenter.TimeRange_fs = presenter.MmToFemtosecond((decimal)nudStageRange.Value);
+
+            UpdateDisplay();
+        }
+
+        private void setMmFromFs()
+        {
+            if (updatingDisplay) return;
+
+            presenter.MoveBy_mm = presenter.FemtosecondToMm((decimal)nudTimeFs.Value);
+            presenter.TimeMove_fs = (decimal)nudTimeFs.Value;
+
+            presenter.MoveRange_mm = presenter.FemtosecondToMm((decimal)nudTimeRangeFs.Value);
+            presenter.TimeRange_fs = (decimal)nudTimeRangeFs.Value;
+
+            UpdateDisplay();
+        }
+
+        private void btnSetMm_Click(object sender, RoutedEventArgs e)
+        {
+            setMmFromFs();
+        }
+
+        private void btnSetMmRange_Click(object sender, RoutedEventArgs e)
+        {
+            setMmFromFs();
+        }
+
+        private void nudTimeFs_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            //setMmFromFs();
+            Log($"{nudTimeFs.Value}");
+        }
+
+        private void nudTimeRangeFs_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            setMmFromFs();
+        }
+        #endregion
+
+        #region Spectrometer Control
+        private void btnChart_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            if (formChart == null || formChart.IsDisposed)
+            formChart = new FormChart(presenter);
+
+            formChart.Show();
+            */
+        }
+
+        private void btnSpectrumFull_Click(object sender, RoutedEventArgs e)
+        {
+            presenter.GetFullSpectrum();
+        }
+
+        private void btnSpectrumRange_Click(object sender, RoutedEventArgs e)
+        {
+            presenter.GetSpectrumAtRange();
+        }
+
+        private void nudCenterWave_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (updatingDisplay) return;
+            presenter.CenterWavelength = (double)nudCenterWave.Value;
+            UpdateDisplay();
+        }
+
+        private void nudWaveRange_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (updatingDisplay) return;
+            presenter.WavelengthRange = (double)nudWaveRange.Value;
+            UpdateDisplay();
+        }
+
+        private void nudIntegrationUs_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (updatingDisplay) return;
+            presenter.IntegrationTime_us = (long)nudIntegrationUs.Value;
+            UpdateDisplay();
+        }
+
+        private void nudWaveInc_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            // NOP
+        }
+        #endregion
+
+        private void tmrMain_Tick(object sender, EventArgs e)
+        {
+            if (presenter.Stage.StageState != MotorState.Stopped)
+            {
+                UpdateStageDisplay();
+            }
+            else
+            {
+                UpdateStageDisplay();
+                tmrMain.IsEnabled = false;
+            }
+        }
+
+        private void btnTestCsv_Click(object sender, RoutedEventArgs e)
+        {
+            presenter.TestWriteToFile();
         }
     }
 }
